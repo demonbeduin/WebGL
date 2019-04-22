b5a5907d547c6d229ede5a5a94e9be89fff7a4ce
demonbeduin <team.sad.info@gmail.com>
2019-04-22 23:03:43 +0300

    init

---
+++ Assets/Scripts/Earth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth : MonoBehaviour
+{
+    public GameObject _earth;
+    public float speed = 20;
+
+    void Start()
+    {
+        _earth.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        _earth.transform.Rotate(Vector3.up * Time.deltaTime * speed);
+    }
+}
