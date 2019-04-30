e37a66f549e1b97e798aa0eadcbeffc88cefcd0a
demonbeduin <team.sad.info@gmail.com>
2019-04-30 14:05:17 +0300

    test 2

---
+++ Assets/Scripts/MouseEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseEvent : MonoBehaviour
+{
+    public GameObject _category;
+
+    private void OnMouseEnter()
+    {
+        print("mouse enter");
+    }
+    private void OnMouseExit()
+    {
+        print("mouse exit");
+    }
+    void Start()
+    {
+        _category.GetComponent<GameObject>();
+    }
+    void Update()
+    {
+        
+    }
+}
